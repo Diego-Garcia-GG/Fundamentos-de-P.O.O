6aa9122389abded5555244b494fb4d329c65d925
Diego-Garcia-GG <123047970@upq.edu.mx>
2025-01-22 14:42:46 -0600

    Estructura de un archivo Java y manejo de cadenas

--- Sintaxis/src/Main.java
+++ Sintaxis/src/Main.java
@@ -1,5 +1,29 @@
+//Importaciones
+
+//Clase principal
 public class Main {
+
+    //Método principal
     public static void main(String[] args) {
+
+        /*Comentario
+        * de
+        * varias
+        * líneas */
+
         System.out.println("Hello, Diego García :)");
+
+        //Cadenas
+        String cadenas = "Diego" + " " + "Garcia" + " " + "García";
+
+        System.out.println(cadenas);
+
+        //Longitud
+        System.out.println(cadenas.length());
+
+        //Extraer posiciones
+        System.out.println(cadenas.substring(2,5));
+        System.out.println(cadenas.substring(2));
+        System.out.println(cadenas.substring(0,5));
     }
 }
