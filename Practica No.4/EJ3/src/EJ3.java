20633f872a96413843c3a7597287b87e9dbed0df
Diego-Garcia-GG <123047970@upq.edu.mx>
2025-01-28 16:18:45 -0600

    3er Ejercicio Terminado

--- Practica No.4/EJ3/src/EJ3.java
+++ Practica No.4/EJ3/src/EJ3.java
@@ -1,5 +1,17 @@
+import java.util.*;
+import javax.swing.JOptionPane;
+
 public class EJ3 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int edad = Integer.parseInt(JOptionPane.showInputDialog(null, "¿Cuál es su edad?"));
+        if(edad<0){
+            JOptionPane.showMessageDialog(null, "ERROR, vuelva a intentarlo...");
+        } else if (edad>=0 && edad<=3) {
+            JOptionPane.showMessageDialog(null, "Usted entra GRATIS!");
+        } else if (edad>=4 && edad<=18) {
+            JOptionPane.showMessageDialog(null, "Usted debe pagar $110");
+        } else if (edad>=19) {
+            JOptionPane.showMessageDialog(null, "Usted debe pagar $190");
+        }
     }
 }
