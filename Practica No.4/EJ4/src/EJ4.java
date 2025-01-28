1606800f5182665febfc64b8afce13db03bb34cf
Diego-Garcia-GG <123047970@upq.edu.mx>
2025-01-28 16:35:43 -0600

    4to Ejercicio Terminado

--- Practica No.4/EJ4/src/EJ4.java
+++ Practica No.4/EJ4/src/EJ4.java
@@ -1,5 +1,18 @@
+import java.util.*;
+import javax.swing.JOptionPane;
+
 public class EJ4 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String Palindromo = JOptionPane.showInputDialog("Introduzca una palabra...");
+        String PalindromoA = "";
+        for(int i=Palindromo.length()-1;i>=0;i--){
+            PalindromoA = PalindromoA + Palindromo.charAt(i);
+        }
+
+        if(Palindromo.equalsIgnoreCase(PalindromoA)){
+            JOptionPane.showMessageDialog(null, "La palabra es palindromo!");
+        } else {
+            JOptionPane.showMessageDialog(null, "La palabra NO es palindoromo");
+        }
     }
 }
