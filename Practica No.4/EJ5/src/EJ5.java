5e1e098cd7977be97b2d515d22a72927e19d74ac
Diego-Garcia-GG <123047970@upq.edu.mx>
2025-01-28 16:48:45 -0600

    5to Ejercicio Terminado

--- Practica No.4/EJ5/src/EJ5.java
+++ Practica No.4/EJ5/src/EJ5.java
@@ -1,5 +1,33 @@
+import java.util.*;
+import javax.swing.JOptionPane;
+
 public class EJ5 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int num1 = Integer.parseInt(JOptionPane.showInputDialog("1er número..."));
+        int num2 = Integer.parseInt(JOptionPane.showInputDialog("2ndo número..."));
+        int num3 = 0;
+        String operador = JOptionPane.showInputDialog("¿Qué operación desea hacer? ( + , - , * , / )");
+
+        switch(operador){
+            case "+":
+                num3 = num1 + num2;
+                JOptionPane.showMessageDialog(null, num3);
+            break;
+            case "-":
+                num3 = num1 - num2;
+                JOptionPane.showMessageDialog(null, num3);
+            break;
+            case "*":
+                num3 = num1 * num2;
+                JOptionPane.showMessageDialog(null, num3);
+            break;
+            case "/":
+                if(num2==0){
+                    JOptionPane.showMessageDialog(null,"La división es indeterminada!");
+                } else {
+                    JOptionPane.showMessageDialog(null, num3);
+            }
+        }
+
     }
 }
