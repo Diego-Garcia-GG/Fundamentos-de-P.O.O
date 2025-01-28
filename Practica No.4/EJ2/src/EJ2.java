9b6cd8c43a11b92be80c1e54caf07f8d43b477d6
Diego-Garcia-GG <123047970@upq.edu.mx>
2025-01-28 16:10:00 -0600

    2ndo Ejercicio Terminado

--- Practica No.4/EJ2/src/EJ2.java
+++ Practica No.4/EJ2/src/EJ2.java
@@ -1,5 +1,13 @@
+import java.util.*;
+import javax.swing.JOptionPane;
+
 public class EJ2 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un número entero..."));
+        if(num%2==0){
+            JOptionPane.showMessageDialog(null,"El número es par!");
+        } else {
+            JOptionPane.showMessageDialog(null, "El número es impar!");
+        }
     }
 }
