aaf37c5e991c90ae95f4acecfd9e3753a5ffc771
Diego-Garcia-GG <123047970@upq.edu.mx>
2025-03-25 16:52:36 -0600

    Práctica No.17 Terminada

---
+++ CRUDUser204/src/Conexión_MySQL.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexión_MySQL {
+    private static final String URL = "jdbc:mysql://localhost:3306/dbpoo?serverTimezone=UTC"; //cuida el nombre de la BD
+    private static final String USUARIO = "root"; // Cambia esto si tienes otro usuario
+    private static final String CONTRASENA = "bd2025+"; // Pon tu contraseña aquí en workbeach
+
+    public static Connection conectar() {
+        
+        Connection conex = null;
+       
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Cargar el driver
+            conex = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+            System.out.println("Conexion exitosa a la base de datos.");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error: No se encontro el driver JDBC.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("🚨 Error en la conexion: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return conex;
+    }
+}
---
+++ CRUDUser204/src/UserCRUD.java
@@ -0,0 +1,61 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+public class UserCRUD {
+    private Connection conexion;
+    
+    //Constructor de la clase que arranca la conexión a BD
+    public UserCRUD(){
+        conexion = Conexión_MySQL.conectar();
+    }
+    
+    //----------------------------------------------------------------------------------------------------------------------------------
+    
+    public boolean InsertUsuario(String nombre, String correo, String contrasena){ //INSERT dentro de MySQL Workbench
+        String insertSQL = " INSERT INTO Usuarios(Nombre, Correo, Contraseña) VALUES(?,?,?)";
+        try{
+            PreparedStatement ps = conexion.prepareStatement(insertSQL); //Aginación de la sentencia y parámetros para su ejecución
+            ps.setString(1, nombre);
+            ps.setString(2, correo);
+            ps.setString(3, contrasena);
+            return ps.executeUpdate()>0;
+        }
+        catch(SQLException e){
+            System.out.print("Error al crear el usuario "+e.getMessage()); //Impresión en consola en caso de que no se ejectue el INSERT
+            return false;
+        }
+    }
+    
+    //----------------------------------------------------------------------------------------------------------------------------------
+    
+    public ResultSet buscarPorID(int id){ //SELECT dentro de MySQL Workbench
+        String sqlBuscar = "SELECT * FROM Usuarios WHERE id_usuario = ?";
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sqlBuscar); //Aginación de la sentencia y parámetros para su ejecución
+            ps.setInt(1, id);
+            return ps.executeQuery();
+        }
+        catch(SQLException e){
+            System.out.print("Error al buscar por ID"+e.getMessage());
+            return null;
+        }
+    }
+    
+    //----------------------------------------------------------------------------------------------------------------------------------
+    
+    public ResultSet obtenerTodos(){ //SELECT general dentro de MySQL Workbench
+        String sqlBuscarTodo = "SELECT * FROM Usuarios";
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sqlBuscarTodo); //Asignación de la sentencia y parámetros para su ejecución
+            return ps.executeQuery();
+        }
+        catch(SQLException e){
+            System.out.print("Error al buscar todos los usuarios"+e.getMessage());
+            return null;
+        }
+    }
+}
+
+
